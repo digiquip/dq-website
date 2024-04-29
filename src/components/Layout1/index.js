96a4022b76720bdfe25fd2ce4905cdf7bae821aa
MZeshanShoukat <zeshanshoukat840@gmail.com>
2024-04-29 17:02:46 +0500

    partners slider with sites links

--- src/components/Layout1/index.js
+++ src/components/Layout1/index.js
@@ -9,6 +9,8 @@ import mainPageContentRecords from "../../../data/mainPageContents";
 import { useState } from "react";
 import { useEffect } from "react";
 
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 function Safeuse(props) {
   const safeuseContent = props.items;
@@ -32,18 +34,47 @@ function ExpertInspections(props) {
 
 
 function OurPartners(props) {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 6,
+      slidesToSlide: 1,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 580 },
+      items: 4,
+      slidesToSlide: 1,
+    },
+    mobile: {
+      breakpoint: { max: 580, min: 0 },
+      items: 2,
+      slidesToSlide: 1,
+    },
+  };
+
   const brandList = props.items;
   return (
     <section className="our-partners-section">
       <h2 className="section-title"><Translate>home.ourpartners.title</Translate>
         <span className="orange">.</span>
       </h2>
-      <div className="our-partner-logo">
-      {brandList.map((props, idx) => (
-          <div className='brand-logo'  key={idx}>
-            <BrandLogo key={idx} {...props} />
-          </div>
-        ))}
+      <div className="">
+        <Carousel
+          responsive={responsive}
+          autoPlay={true}
+          autoPlaySpeed={2000}
+          infinite={true}
+          arrows={false}
+        >
+            {brandList.map((props, idx) => (
+              <div className='brand-logo brand-carousel-item'  key={idx}>
+                <a href={props.link} target="_blank">
+                  <BrandLogo key={idx} {...props} />
+                </a>
+              </div>
+            ))}
+
+        </Carousel>
         {/* {brandList.map((props, idx) => (
           <div className='brand-logo'>
             <BrandLogo key={idx} {...props} />
