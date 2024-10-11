1d4569ca3289642f75a81815882d6fa1878d60cd
MZeshanShoukat <zeshanshoukat840@gmail.com>
2024-10-11 18:30:44 +0500

    Add Svg in contact us button

--- src/components/ContactUsDropdown/index.js
+++ src/components/ContactUsDropdown/index.js
@@ -7,7 +7,9 @@ export default function ButtonDropdown() {
     return (
         <div className="contactUsDropdown">
             <div className="dropdown">
-                <button className="btn red-btn dropdown-btn"><Translate>navbar.label.contact</Translate></button>
+                <button className="btn red-btn dropdown-btn">
+                    <img src="/icons/phone.svg" alt="Contact Us" />
+                </button>
                 <div className="dropdown-content">
                     <Link href="https://us21.list-manage.com/contact-form?u=45bf490556cbc57d81d50c4f6&form_id=fb2b014417ac7312f80a8bb815f52ab5" target="_blank"><Translate>navbar.label.contact_us</Translate></Link>
                     <Link href="/about"><Translate>navbar.label.book_meeting</Translate></Link>
