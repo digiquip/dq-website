fbb8c0809dea78f0fa023040fd753b558dfe81b4
Irina <i.grebennikova@elfsight.com>
2025-04-07 11:38:34 +0400

    add contact form

--- src/pages/bedrift/index.js
+++ src/pages/bedrift/index.js
@@ -4,6 +4,7 @@ import ContactUs from '../../components/ContactUs/index.js';
 import Banner from '../../components/Banner/index.js';
 import bedriftRecords from "../../../data/bedriftContents.js";
 import { useEffect, useState } from 'react';
+import { ElfsightWidget } from 'react-elfsight-widget';
 
 function BedriftBanner(props) {
   const bedriftContent = props.items;
@@ -57,7 +58,8 @@ export default function Bedrift() {
     <MainLayout>
       {bedrift && <BedriftBanner items={bedrift}/>}
       {bedriftgroups && <BedriftGroups items={bedriftgroups}/>}
-      {bedriftbenefits && <BedriftBenefits items={bedriftbenefits}/>}
+      {bedriftbenefits && <BedriftBenefits items={bedriftbenefits} />}
+      <ElfsightWidget widgetId="8601984d-0d6c-4f1a-bb3f-495f303d2c44" style={{padding: "50px 1rem 0"}}/>
       <CompetenceContactus />
     </MainLayout>
   );
