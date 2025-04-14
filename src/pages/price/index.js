d3927e95b29e5cd0f2babe97c7f37a5492c8f915
Cathrine <153903367+CathrineDQ@users.noreply.github.com>
2025-04-14 22:07:52 +0200

    Blog article Geir Schau and updated Price matrix

--- src/pages/price/index.js
+++ src/pages/price/index.js
@@ -19,14 +19,17 @@ function PricePageBanner() {
     };
   }, []);
 
+//elfsight number 1 is the price matrix, number 2 is the pop-up demo form:
+
   return (
     <>
       <section className='homepage-banner'>
         <div className={clsx('hero-25 hero hero--primary heroBanner')}>
           <div className="container">
           <h1 className='section-title'>Priser<span className='orange'>.</span></h1>
-          <h4 className='section'>Her er våre priser. Husk at du alltid kan ta kontakt for en demo.</h4>
-            <div className="elfsight-app-81cbe388-1724-4e56-b24d-e9cbcb69c3f4" data-elfsight-app-lazy />
+          <h4 className='section'>Her er våre priser. Ta gjerne kontakt for en demo!</h4>
+            <div className="elfsight-app-ff89e3a5-ee09-41f1-8424-30531e5f84c0" data-elfsight-app-lazy />
+            <div class="elfsight-app-954fa691-f581-4d70-a48d-6ba7822b0756" data-elfsight-app-lazy></div>
             <div className="row">
             </div>
           </div>
@@ -34,7 +37,6 @@ function PricePageBanner() {
         </div>
       </section>
     </>
-
   );
 }
 
@@ -55,7 +57,6 @@ function AboutIntegrations() {
 export default function About() {
   return (
     <MainLayout>
-
       <PricePageBanner />
       <AboutIntegrations />
       <ContactUs />
