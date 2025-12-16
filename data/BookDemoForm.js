a6d26755cae566f925bad0ecc703b2fccd9947d0
Cathrine <153903367+CathrineDQ@users.noreply.github.com>
2025-12-16 13:56:45 +0100

    Ny side: /book-demo-kvipp og Sansita font

---
+++ data/BookDemoForm.js
@@ -0,0 +1,7 @@
+const bookDemoForm = `
+<!-- Elfsight Contact Form | Book demo med tid - egen side -->
+<script src="https://elfsightcdn.com/platform.js" async></script>
+<div class="elfsight-app-906fbdca-e891-4762-be26-edd34f63e6d0" data-elfsight-app-lazy></div>
+`;
+export default bookDemoForm;
+
